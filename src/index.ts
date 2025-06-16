ad042ae8a78ee6c5b35d65cdc55b940750dad27f
weili.dai <weili.dai@gumtree.com>
2025-06-16 12:05:34 +0100

    Hierarchy view

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './utils/list-of-objects';
-export * from './utils/list-of-primitives';
+export * from './utils/lists/list-of-objects';
+export * from './utils/lists/list-of-primitives';
 // Add more exports here as you create more utility modules
